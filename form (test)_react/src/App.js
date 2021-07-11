99abab48981d5bd3a7850ddc9629bc2657c5276f
Dariia89 <alefed89@mail.ru>
2021-07-11 21:08:27 +0300

    new project added (react-form)

---
+++ form (test)_react/src/App.js
@@ -0,0 +1,13 @@
+import MainForm from './components/MainForm';
+
+function App() {
+  return (
+    <>
+      <section className="main-form">
+        <MainForm />
+      </section>
+    </>
+  );
+}
+
+export default App;
